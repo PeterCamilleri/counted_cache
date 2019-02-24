d28a53c2ae985c79bd971bd454d0f55610dd87e5
PeterCamilleri <peter.c.camilleri@gmail.com>
2019-02-24 10:57:19 -0500

    Fix phase 4

--- test/counted_cache_test.rb
+++ test/counted_cache_test.rb
@@ -1,11 +1,22 @@
-require "test_helper"
+require_relative '../lib/counted_cache'
+require          'minitest/autorun'
+require          'minitest_visible'
 
 class CountedCacheTest < Minitest::Test
   def test_that_it_has_a_version_number
-    refute_nil ::CountedCache::VERSION
+    refute_nil(::CountedCache::VERSION)
+    assert(::CountedCache::VERSION.frozen?)
+    assert(::CountedCache::VERSION.is_a?(String))
+    assert(/\A\d+\.\d+\.\d+/ =~ ::CountedCache::VERSION)
+  end
+
+  def test_that_it_has_a_description
+    refute_nil(::CountedCache::DESCRIPTION)
+    assert(::CountedCache::DESCRIPTION.frozen?)
+    assert(::CountedCache::DESCRIPTION.is_a?(String))
   end
 
   def test_it_does_something_useful
-    assert false
+    assert(false)
   end
 end
--- test/test_helper.rb
+++
@@ -1,4 +0,0 @@
-$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
-require "counted_cache"
-
-require "minitest/autorun"
