54fa7af25b5aeed63bf303ed18613526e5ee9459
PeterCamilleri <peter.c.camilleri@gmail.com>
2019-02-24 11:41:00 -0500

    Updated counted cache class description.

--- lib/counted_cache.rb
+++ lib/counted_cache.rb
@@ -1,4 +1,6 @@
-# counted_cache
+# The counted_cache class. A cache for use in cases where data is mostly only
+# read and where the cost of rebuilding that data is high, and yet, the size
+# of the data is too large to keep all of it in RAM all of the time.
 
 require_relative 'counted_cache/object'
 require_relative 'counted_cache/counted_cache_item'
