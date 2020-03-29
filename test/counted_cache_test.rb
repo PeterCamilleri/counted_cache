1c13146a5bb12a9ad1bb1d006974693ec5e1674e
PeterCamilleri <peter.c.camilleri@gmail.com>
2020-03-29 13:22:04 -0400

    Removed dependency on minitest_visible for issue #2

--- test/counted_cache_test.rb
+++ test/counted_cache_test.rb
@@ -1,8 +1,8 @@
 require_relative '../lib/counted_cache'
 require          'minitest/autorun'
-require          'minitest_visible'
 
 class CountedCacheTest < Minitest::Test
+
   def test_that_it_has_a_version_number
     refute_nil(::CountedCache::VERSION)
     assert(::CountedCache::VERSION.frozen?)
