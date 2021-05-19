aa3f302470a9d837691e3c1efdae28e9c638d29f
PeterCamilleri <peter.c.camilleri@gmail.com>
2021-05-19 11:22:32 -0400

    Updated bundler dependency for next version

--- lib/counted_cache/version.rb
+++ lib/counted_cache/version.rb
@@ -1,5 +1,5 @@
 class CountedCache
-  VERSION = "0.2.2".freeze
+  VERSION = "0.2.3".freeze
 
   DESCRIPTION = "counted_cache: A cache for mostly read data with a high cost of retrieval.".freeze
 end
